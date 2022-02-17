8fdaa9b79ff2b9e5bce3d81e1bc4936433d2b6b3
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-17 15:01:28 +0100

    Introducing iOS cupertino date picker widget to out app

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -20,6 +21,15 @@ class _PriceScreenState extends State<PriceScreen> {
       dropdownItems.add(newItem);
     }
     return dropdownItems;
+  }
+
+  List<Text> getPickerItems() {
+    List<Text> pickerItems = [];
+
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+    return pickerItems;
   }
 
   @override
@@ -58,14 +68,12 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: selectedCurrency,
-              items: getDropdownItems(),
-              onChanged: (value) {
-                setState(() {
-                  selectedCurrency = value;
-                });
+            child: CupertinoPicker(
+              itemExtent: 32,
+              onSelectedItemChanged: (selectedIndex) {
+                print(selectedIndex);
               },
+              children: getPickerItems(),
             ),
           ),
         ],
@@ -73,3 +81,14 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 }
+
+/*
+DropdownButton<String>(
+value: selectedCurrency,
+items: getDropdownItems(),
+onChanged: (value) {
+setState(() {
+selectedCurrency = value;
+});
+},
+),*/
